cffa3beb7b852a9a40ebdb350a8413a94e49e33f
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2024-10-06 22:36:21 +0530

    Contact Me section linked email I'd

--- client/main.js
+++ client/main.js
@@ -58,3 +58,62 @@ const typed = new Typed('.multiple-text', {
     loop: true,
 });
 
+const form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email")
+const phone = document.getElementById("phone")
+const subjects = document.getElementById("subject")
+const mess = document.getElementById("message")
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}
+    <br> Phone Number: ${phone.value} <br> Message: ${mess.value}`;
+    
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "udhayaaudhayaa987@gmail.com",
+        Password : "63B5AA070C1BE6D5A8E5B1790BA060CD114A",
+        To : 'udhayaaudhayaa987@gmail.com',
+        From : "udhayaaudhayaa987@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "OK"){
+            Swal.fire({
+                title: "Success!",
+                text: "Message Sent Successfully",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+/*function checkInputs() {
+    const items = document.getElementById(".item");
+
+    for(const item of items) {
+        if(item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        item.addEventListener("keyup" ,() => {
+            if(item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("remove");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}*/
+
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    //checkInputs();
+    sendEmail();
+});
